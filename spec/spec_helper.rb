921b475e49c8d309e50a9456cbf81304ea25d768
Paweł Pacana <pawel.pacana@gmail.com>
2012-01-21 09:55:44 +0100

    include method returning app instance to any spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,15 @@ require 'bundler/setup'
 require 'webmachine/test'
 require 'fixtures/test_resource'
 
+module WebmachineTestApplication
+  def app
+    @app ||= Webmachine::Application.new.tap do |test_app|
+      test_app.add_route(['*'], TestResource)
+    end
+  end
+end
+
+RSpec.configure do |c|
+  c.include WebmachineTestApplication
+end
 
